5406f321677796db8e5752499c2ec5108cdd0323
Gabriel Nunes <gabrielnunesdacostaanselmo@gmail.com>
2025-05-04 20:03:31 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Digite um número: ");
+int numero   = int.Parse(Console.ReadLine()!);
+if(numero % 2 == 0){
+Console.WriteLine($"{numero} é par");
+}
+else{
+    Console.Write($"{numero} é ímpar");
+}
